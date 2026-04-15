025bb443ad6b2bb26660483c9dc3aadacdd0bc31
Megaplex Dev <pokusafox@megaplexcon.org>
2026-04-14 22:18:57 -0400

    Avoid weird characters in b64encoding

--- src/RandomString.php
+++ src/RandomString.php
@@ -1,14 +1,22 @@
 <?php
 namespace Scurriio\Utils;
 
+use SplFixedArray;
+
 class RandomString{
+
+    const ALPHA = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
     /**
      * Generate a random string of given length
      */
     static function make(int $length=64): string{
-        $b64str = base64_encode(random_bytes($length));
-        $b64str = substr($b64str, 0, $length);
-        return $b64str;
+        $str = new SplFixedArray($length);
+        $alphaLen = strlen(RandomString::ALPHA);
+        for($i = 0; $i < $length; $i++){
+            $str[$i] = RandomString::ALPHA[random_int(0, $alphaLen - 1)];
+        }
+        return join('', $str->toArray());
     }
 }
 ?>
